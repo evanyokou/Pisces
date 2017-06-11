eb7d661eaea18dfdf6a25888186f7fb89e4259c3
yokou <evanyokou@sina.com>
2017-06-11 22:07:07 +0800

    Add assets and base class files

---
+++ src/main/kotlin/com/yh/web/service/impl/BaseService.kt
@@ -0,0 +1,11 @@
+package com.yh.web.service.impl
+
+import com.yh.web.service.IBaseService
+import org.springframework.stereotype.Service
+
+/**
+ * Created by Ypc on 2017/06/11.
+ */
+@Service
+class BaseService : IBaseService{
+}
